954f26b11360bcd9aa40bc491587c14cbf959a39
smith <smithbabawande@gmail.com>
2023-12-18 21:57:22 +0100

    env file added

--- server/config.py
+++ server/config.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_jwt_extended import JWTManager
 
+
 app = Flask(__name__)
 app.secret_key = 'secret key'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI")
@@ -17,6 +18,9 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 metadata = MetaData(naming_convention={
         "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
     })
+
+
+   
     
 db = SQLAlchemy(metadata=metadata)
 
