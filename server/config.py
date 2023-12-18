04fe3ed78ae8d33c25c75de45f49fe6eb3496165
smith <smithbabawande@gmail.com>
2023-12-18 10:34:25 +0100

    refactoring

--- server/config.py
+++ server/config.py
@@ -7,25 +7,27 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_jwt_extended import JWTManager
 
+app = Flask(__name__)
+app.secret_key = 'secret key'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI")
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+
+
 metadata = MetaData(naming_convention={
         "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
     })
     
 db = SQLAlchemy(metadata=metadata)
-app = Flask(__name__)
-api = Api(app)
-
-app.secret_key = 'secret key'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI")
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
+
+
 
 bcrypt = Bcrypt(app)
 
-        
+api = Api(app)
 
 jwt = JWTManager(app)
 
